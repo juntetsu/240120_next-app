30aae3064d7e4d5d07084a82f1219ea1f4ec5793
jun <juntetsu.1012@gmail.com>
2024-01-20 10:44:06 +0900

    予約ファイル作成

---
+++ app/components/navigation.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+// ナビゲーション
+const Navigation = () => {
+  return (
+    <header className="shadow-lg shadow-gray-100">
+      <div className="container mx-auto flex max-w-screen-sm items-center justify-between px-1 py-5">
+        <Link href="/" className="cursor-pointer text-xl font-bold">Pan&apos;s App</Link>
+      </div>
+    </header>
+  );
+};
+
+export default Navigation;
